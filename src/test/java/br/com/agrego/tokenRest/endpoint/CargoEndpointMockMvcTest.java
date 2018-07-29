d8966bb88f8511db93d25972142ba682bb9057b7
andreygalassi <andreygalassi@gmail.com>
2018-07-29 15:33:33 -0300

    convertendo para mockmvc

--- src/test/java/br/com/agrego/tokenRest/endpoint/CargoEndpointMockMvcTest.java
+++ src/test/java/br/com/agrego/tokenRest/endpoint/CargoEndpointMockMvcTest.java
@@ -1,11 +1,9 @@
 package br.com.agrego.tokenRest.endpoint;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Arrays;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,31 +13,25 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.google.gson.Gson;
+
 import br.com.agrego.tokenRest.model.Cargo;
+import br.com.agrego.tokenRest.model.acesso.Permissoes;
 import br.com.agrego.tokenRest.repository.CargoRepositoryImp;
 
 @RunWith(SpringRunner.class)
 //Inicia o endpoint com uma porta aleatória, evita problemas quando o ambiente já está em pé
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc//utilizado para o mockMvc
-public class CargoEndpointTest {
+public class CargoEndpointMockMvcTest {
 
-	//2 ferramentas para testar endpoint (resttemplate e mockmvc) exemplos com as 2
-	@Autowired
-	private TestRestTemplate restTemplate;
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -48,15 +40,6 @@ public class CargoEndpointTest {
 	@MockBean
 	private CargoRepositoryImp cargoRepo;
 	
-	//Configura o restTemplate para usar autentitação basica
-	@TestConfiguration
-	static class Config{
-		@Bean
-		public RestTemplateBuilder restTemplateBuilder(){
-			return new RestTemplateBuilder().basicAuthorization("admin", "123");
-		}
-	}
-	
 	@Before
 	public void setup(){
 		Cargo cargo = Cargo.newInstance(1l,"Cargo mock 1");
@@ -64,92 +47,121 @@ public class CargoEndpointTest {
 	}
 	
 	@Test
-	public void deveRetornarStatusCode401QuandoListarCargoUsandoUsuarioSenhaInvalidos(){
-		restTemplate = restTemplate.withBasicAuth("1", "1");
-		ResponseEntity<String> res = restTemplate.getForEntity("/v1/cargos", String.class);
-		
-		assertThat(res.getStatusCodeValue()).isEqualTo(401);
+	public void deveRetornarStatusCode401QuandoListarCargoUsandoUsuarioNaoLogado() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos"))
+			.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+	}
+	@Test
+	@WithMockUser(username="xx",password="xx",authorities={"OUTRO"})
+	public void deveRetornarStatusCode403QuandoListarCargoUsandoUsuarioSemAutorizacao() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos"))
+			.andExpect(MockMvcResultMatchers.status().isForbidden());
 	}
 	
 	@Test
-	public void deveRetornarStatusCode401QuandoBuscaCargoPorIdUsandoUsuarioSenhaInvalidos(){
-		restTemplate = restTemplate.withBasicAuth("1", "1");
-		ResponseEntity<String> res = restTemplate.getForEntity("/v1/cargos/1", String.class);
-		
-		assertThat(res.getStatusCodeValue()).isEqualTo(401);
+	@WithMockUser(username="xx",password="xx",authorities={"OUTRO"})
+	public void deveRetornarStatusCode401QuandoBuscaCargoPorIdUsandoUsuarioSemAutorizacao() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos/1"))
+			.andExpect(MockMvcResultMatchers.status().isForbidden());
 	}
 	
 	@Test
-	public void deveRetornarStatusCode200QuandoListarCargoUsandoMock(){
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_READ})
+	public void deveRetornarStatusCode200QuandoListarCargo() throws Exception{
 		List<Cargo> listaMokada = Arrays.asList(Cargo.newInstance(1l,"Cargo mock 1"),Cargo.newInstance(2l,"Cargo mock 2"));
 		BDDMockito.when(cargoRepo.findAll()).thenReturn(listaMokada);
-		ResponseEntity<String> res = restTemplate.getForEntity("/v1/cargos", String.class);
-		
-		assertThat(res.getStatusCodeValue()).isEqualTo(200);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].id", Matchers.is(1)))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[0].titulo", Matchers.is("Cargo mock 1")))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[1].id", Matchers.is(2)))
+			.andExpect(MockMvcResultMatchers.jsonPath("$[1].titulo", Matchers.is("Cargo mock 2")))
+			;
 	}
 	
 	@Test
-	public void deveRetornarStatusCode200QuandoBuscaCargoPorIdUsandoMock(){
-		ResponseEntity<String> res = restTemplate.getForEntity("/v1/cargos/{id}", String.class, 1l);
-		
-		assertThat(res.getStatusCodeValue()).isEqualTo(200);
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_READ})
+	public void deveRetornarStatusCode200QuandoBuscaCargoPorId() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos/{id}",1))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.titulo", Matchers.is("Cargo mock 1")))
+			;
 	}
 	
 	@Test
-	public void deveRetornarStatusCode404QuandoBuscaCargoPorIdInvalido(){
-		ResponseEntity<Cargo> res = restTemplate.getForEntity("/v1/cargos/{id}", Cargo.class, -1);
-		assertThat(res.getStatusCodeValue()).isEqualTo(404);
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_READ})
+	public void deveRetornarStatusCode404QuandoBuscaCargoPorIdInvalido() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/v1/cargos/{id}",-1))
+			.andExpect(MockMvcResultMatchers.status().isNotFound())
+			;
 	}
 	
 	@Test
-	public void deveRetornarStatusCode200QuandoDeletarCargoPorIdComUsuarioAdmin(){
-		BDDMockito.doNothing().when(cargoRepo).delete(1l);
-		ResponseEntity<String> exc = restTemplate.exchange("/v1/cargos/{id}", HttpMethod.DELETE,null,String.class,1l);
-		Assertions.assertThat(exc.getStatusCodeValue()).isEqualTo(200);
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_DELETE})
+	public void deveRetornarStatusCode200QuandoDeletarCargoPorId() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.delete("/v1/cargos/{id}",1))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.titulo", Matchers.is("Cargo mock 1")))
+			;
 	}
 	
 	@Test
-	public void deveRetornarStatusCode404QuandoDeletarCargoPorIdInexistenteComUsuarioAdmin(){
-		BDDMockito.doNothing().when(cargoRepo).delete(1l);
-		ResponseEntity<String> exc = restTemplate.exchange("/v1/cargos/{id}", HttpMethod.DELETE,null,String.class,-1l);
-		Assertions.assertThat(exc.getStatusCodeValue()).isEqualTo(404);
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_DELETE})
+	public void deveRetornarStatusCode404QuandoDeletarCargoPorIdInexistente() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.delete("/v1/cargos/{id}",-1))
+			.andExpect(MockMvcResultMatchers.status().isNotFound())
+			;
 	}
 	
+//	//TODO aprender a tratar os teste com roles dinamicas
 	@Test
-	@WithMockUser(username="xx",password="xx",authorities={"CARGO_DELETAR"})//,roles={"USER","ADMIN"}
-	public void deveRetornarStatusCode404QuandoDeletarCargoPorIdInexistenteComUsuarioAdminUsandoMockmvc() throws Exception{
-		BDDMockito.doNothing().when(cargoRepo).delete(1l);
-//		ResponseEntity<String> exc = restTemplate.exchange("/v1/cargos/{id}", HttpMethod.DELETE,null,String.class,-1l);
-//		Assertions.assertThat(exc.getStatusCodeValue()).isEqualTo(404);
-		mockMvc.perform(MockMvcRequestBuilders.delete("/v1/cargos/{id}",-1l))
-				.andExpect(MockMvcResultMatchers.status().isNotFound());
-	}
-	
-	//TODO aprender a tratar os teste com roles dinamicas
-	@Test
-	@WithMockUser(username="xx",password="xx",authorities={"CARGO_OUTROS"})//,roles={"USER"}
-	public void deveRetornarStatusCode403QuandoDeletarCargoPorIdInexistenteComUsuarioSemPermissaoAdminUsandoMockmvc() throws Exception{
+	@WithMockUser(username="xx",password="xx",authorities={"CARGO_OUTROS"})
+	public void deveRetornarStatusCode403QuandoDeletarCargoPorIdInexistenteComUsuarioSemPermissao() throws Exception{
 		BDDMockito.doNothing().when(cargoRepo).delete(1l);
 		mockMvc.perform(MockMvcRequestBuilders.delete("/v1/cargos/{id}",-1l))
 				.andExpect(MockMvcResultMatchers.status().isForbidden());
 	}
 	
 	@Test
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_CREATE})
 	public void deveRetornarStatusCode400QuandoSalvarCargoComTituloNull() throws Exception{
-		Cargo cargo = Cargo.newInstance(3l,null);
+		Cargo cargo = Cargo.newInstance(null,null);
 		BDDMockito.when(cargoRepo.save(cargo)).thenReturn(cargo);
-		ResponseEntity<String> res = restTemplate.postForEntity("/v1/cargos", cargo, String.class);
-		assertThat(res.getStatusCodeValue()).isEqualTo(400);
-		assertThat(res.getBody()).contains("fieldMessage","Título do cargo é obrigatório");
+		Gson gson = new Gson();
+		mockMvc.perform(
+					MockMvcRequestBuilders.post("/v1/cargos")
+					.contentType("application/json;charset=UTF-8")
+					.content(gson.toJson(cargo)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest())
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.fieldMessage", Matchers.is("Título do cargo é obrigatório")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is("Erro na validação de campo")))
+				;
 	}
 	
 	@Test
+	@WithMockUser(username="xx",password="xx",authorities={Permissoes.CARGO_CREATE})
 	public void deveRetornarStatusCode201QuandoSalvarCargo() throws Exception{
 		Cargo cargo = Cargo.newInstance(3l,"Cargo mock 1");
 		BDDMockito.when(cargoRepo.save(cargo)).thenReturn(cargo);
-		ResponseEntity<Cargo> res = restTemplate.postForEntity("/v1/cargos", cargo, Cargo.class);
-		assertThat(res.getStatusCodeValue()).isEqualTo(201);
-		assertThat(res.getBody().getId()).isNotNull();
+
+		Gson gson = new Gson();
+		mockMvc.perform(
+					MockMvcRequestBuilders.post("/v1/cargos")
+					.contentType("application/json;charset=UTF-8")
+					.content(gson.toJson(cargo)))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(3)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.titulo", Matchers.is("Cargo mock 1")))
+				;
 	}
 	
 }
